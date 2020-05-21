80c7e07c2faa0ac93f6274a3e86088c56d1f11c1
Wash-for-app <62841508+Wash-for-app@users.noreply.github.com>
2020-05-20 22:03:51 -0300

    Criação das pag. busca e serviço, alteração home.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,22 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', 
+    redirectTo: 'home', 
+    pathMatch: 'full' 
+  },
+
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  
+  {
+    path: 'busca',
+    loadChildren: () => import('./busca/busca.module').then( m => m.BuscaPageModule)
+  },
+  {
+    path: 'servico',
+    loadChildren: () => import('./servico/servico.module').then( m => m.ServicoPageModule)
+  },
+
 ];
 
 @NgModule({
